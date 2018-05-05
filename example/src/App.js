4f1e1ecb5f6b5bb66817443c578f15c626231acc
igor.kruglenko <igor.kruglenko@uvoteam.com>
2018-05-06 01:03:59 +0300

    Gitacora saved 1 changed files

--- example/src/App.js
+++ example/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 
 import ExampleComponent from "simple-spa-lib";
 import HelloWorld from "simple-spa-lib";
+import Button from "../../src/components/Button";
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <ExampleComponent text="Modern React component module" />
+        <Button />
       </div>
     );
   }
