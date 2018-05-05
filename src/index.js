879a68edfe5eaaef6978472521a5689250ad9e60
igor.kruglenko <igor.kruglenko@uvoteam.com>
2018-05-06 00:53:29 +0300

    Gitacora saved 1 changed files

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
 
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import Button from "./components/Button";
+import *  from "./components/Button";
 
 import styles from "./styles.css";
 
