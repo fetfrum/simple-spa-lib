90e7385f584dd4fafe5cb4ca636706aef90c7b22
igor.kruglenko <igor.kruglenko@uvoteam.com>
2018-05-07 03:35:33 +0300

    Gitacora saved 1 changed files

--- example/src/App.js
+++ example/src/App.js
@@ -2,9 +2,6 @@ import React, { Component } from "react";
 import { Button, Section } from "simple-spa-lib";
 
 export default class App extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  };
   render() {
     return (
       <div>
