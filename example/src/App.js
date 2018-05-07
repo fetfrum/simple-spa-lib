ebbc54e37403b5c551163c57a2cfc5d914573cd3
igor.kruglenko <igor.kruglenko@uvoteam.com>
2018-05-07 03:23:36 +0300

    Gitacora saved 1 changed files

--- example/src/App.js
+++ example/src/App.js
@@ -5,7 +5,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <ExampleComponent text="Modern React component module" />
+        <Sectinon />
         <Button />
       </div>
     );
